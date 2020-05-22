8ff35e28453ac19da61a1678fe624e1cfd4e3f0d
Jordy Mota <42422126+JordyMota@users.noreply.github.com>
2020-05-22 11:24:56 -0300

    Finalizando o 'cerebro' (#9)

--- processos_decisorios/game/js/components/pc_brain.js
+++ processos_decisorios/game/js/components/pc_brain.js
@@ -59,8 +59,11 @@ function setPriority() {
 						(item.x + sumBackX + sumBackX) === block.x && (item.y + sumBackY + sumBackY) === block.y
 					));
 					if (possFront || possBack) {
-						item.priority += 200;
+						item.priority += 1000;
 						return;
+					}
+					if (usersInfo.pc.points === 1) {
+						item.priority += 800;
 					}
 					item.priority += 30;
 					return;
@@ -74,8 +77,11 @@ function setPriority() {
 							(item.x + sumX + sumX + sumX) === block.x && (item.y + sumY + sumY + sumY) === block.y
 						));
 						if (poss) {
-							item.priority += 70;
+							item.priority += 1000;
 							return;
+						}
+						if (usersInfo.pc.points === 1) {
+							item.priority += 800;
 						}
 						item.priority += 30;
 						return;
@@ -99,8 +105,11 @@ function setPriority() {
 							(item.x + sumBackX + sumBackX + sumBackX) === block.x && (item.y + sumBackY + sumBackY + sumBackY) === block.y
 						));
 						if (poss) {
-							item.priority += 200;
+							item.priority += 1000;
 							return;
+						}
+						if (usersInfo.pc.points === 1) {
+							item.priority += 800;
 						}
 						item.priority += 30;
 						return;
@@ -116,7 +125,89 @@ function setPriority() {
 					return;
 				}
 			}
+			const verifyEnemy = ()=> {
+				let [possibilityFront] = usersInfo.user.blocks.filter(block => (
+					item.x + sumX === block.x && item.y + sumY === block.y
+				));
+				let [possibilityBack] = usersInfo.user.blocks.filter(block => (
+					item.x + sumBackX === block.x && item.y + sumBackY === block.y
+				));
+				if (possibilityFront && possibilityBack) {
+					let [possFront] = usersInfo.user.blocks.filter(block => (
+						(item.x + sumX + sumX) === block.x && (item.y + sumY + sumY) === block.y
+					));
+					let [possBack] = usersInfo.user.blocks.filter(block => (
+						(item.x + sumBackX + sumBackX) === block.x && (item.y + sumBackY + sumBackY) === block.y
+					));
+					if (possFront || possBack) {
+						item.priority += 200;
+						return;
+					}
+					if (usersInfo.user.points === 1) {
+						item.priority += 200;
+					}
+					item.priority += 2;
+					return;
+				}
+				if (possibilityFront) {
+					let [possibility] = usersInfo.user.blocks.filter(block => (
+						(item.x + sumX + sumX) === block.x && (item.y + sumY + sumY) === block.y
+					));
+					if (possibility) {
+						let [poss] = usersInfo.user.blocks.filter(block => (
+							(item.x + sumX + sumX + sumX) === block.x && (item.y + sumY + sumY + sumY) === block.y
+						));
+						if (poss) {
+							item.priority += 200;
+							return;
+						}
+						if (usersInfo.user.points === 1) {
+							item.priority += 200;
+						}
+						item.priority += 2;
+						return;
+					}
+					let [emptyNext] = tableInfo.filter(block => (
+						(item.x + sumBackX) === block.x && (item.y + sumBackY) === block.y && !block.blocked && !block.filled
+					));
+					if (emptyNext) {
+						item.priority += 1;
+						return;
+					}
+					item.priority += 0;
+					return;
+				}
+				if (possibilityBack) {
+					let [possibility] = usersInfo.user.blocks.filter(block => (
+						(item.x + sumBackX + sumBackX) === block.x && (item.y + sumBackY + sumBackY) === block.y
+					));
+					if (possibility) {
+						let [poss] = usersInfo.user.blocks.filter(block => (
+							(item.x + sumBackX + sumBackX + sumBackX) === block.x && (item.y + sumBackY + sumBackY + sumBackY) === block.y
+						));
+						if (poss) {
+							item.priority += 200;
+							return;
+						}
+						if (usersInfo.user.points === 1) {
+							item.priority += 200;
+						}
+						item.priority += 2;
+						return;
+					}
+					let [emptyNext] = tableInfo.filter(block => (
+						(item.x + sumX) === block.x && (item.y + sumY) === block.y && !block.blocked && !block.filled
+					));
+					if (emptyNext) {
+						item.priority += 1;
+						return;
+					}
+					item.priority += 0;
+					return;
+				}
+			}
 			verifyUser();
+			verifyEnemy();
 		});
 	});
 }
